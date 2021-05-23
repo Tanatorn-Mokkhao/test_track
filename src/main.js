a928bb364d2ebf30b07ad0f9fc406b01e6f7a6c8
Tanatorn-Mokkhao <6106021621041@fitm.kmutnb.ac.th>
2021-05-23 12:13:53 +0700

    first commit

--- src/main.js
+++ src/main.js
@@ -4,8 +4,17 @@ import VueGtag from "vue-gtag";
 import vuetify from "./plugins/vuetify";
 
 Vue.use(VueGtag, {
-  config: { id: "UA-197670238-1" },
+  config: { id: "G-E7H9B0YMQ6" },
 });
+export default {
+  name: "MyComponent",
+
+  methods: {
+    login() {
+      this.$gtag.event("login", { method: "Google" });
+    },
+  },
+};
 
 Vue.config.productionTip = false;
 
